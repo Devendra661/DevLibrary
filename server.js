6931a2948e0a49e774529ac5a2bcfdc7379e54b8
dev <dk4200720@gmail.com>
2025-09-11 00:28:14 +0530

    fix: backend only serve api routes

--- server.js
+++ server.js
@@ -456,11 +456,11 @@ app.use(express.static('public'));
 
 // Serve React in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "dist")));
+  app.use(express.static(path.join(__dirname, "..", "dist")));
 
   // 👇 use a safe wildcard
   app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
   });
 }
 
