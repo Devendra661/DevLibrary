935fbf2e758a91e1317e618a442e6bde74867269
dev <dk4200720@gmail.com>
2025-09-11 00:21:08 +0530

    Use process.env.PORT for Render deployment

--- server.js
+++ server.js
@@ -459,7 +459,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "dist")));
 
   // 👇 use a safe wildcard
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
 }
