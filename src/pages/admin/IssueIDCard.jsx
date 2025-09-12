99554f25c042e3394a32899c435e31fb0b18bcdd
dev <dk4200720@gmail.com>
2025-09-12 23:37:07 +0530

    hello

--- src/pages/admin/IssueIDCard.jsx
+++ src/pages/admin/IssueIDCard.jsx
@@ -110,7 +110,7 @@ export default function IssueIDCard() {
               <div className="w-32 h-32 md:w-36 md:h-36 rounded-full border-2 border-red-500 flex items-center justify-center overflow-hidden bg-gray-200">
                 {studentToDisplay.image ? (
                   <img
-                    src={studentToDisplay.image}
+                    src={`${import.meta.env.VITE_API_URL.replace("/api", "")}${studentToDisplay.image}`}
                     alt="Profile"
                     className="w-full h-full object-cover"
                   />
